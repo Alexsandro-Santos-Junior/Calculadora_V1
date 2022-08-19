6b206aa5814934681ecaaeedfa2817cde29ae4a6
Alexsandro-Santos-Junior <alexsandro_azuras@hotmail.com>
2022-08-19 12:06:14 -0300

    Calculado_V1

---
+++ Calculadora_V1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha o primeiro numero e aguarde!");
+                int entrada = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escolha o segundo numero e aguarde!");
+                int entrada2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escolha um Operador (+,-,*,/)");
+                var escolha =Console.ReadLine();
+            
+            switch (escolha)
+              
+            {
+
+                case "+":
+                    Console.WriteLine($" O resultado é {entrada + entrada2}");
+                    break;
+                case "-":
+                    Console.WriteLine($" O resultado é {entrada - entrada2}");
+                    break;
+                case "*":
+                    Console.WriteLine($" O resultado é {entrada * entrada2}");
+                    break;
+                case "/":
+                    Console.WriteLine($" O resultado é {entrada / entrada2}");
+                    break;
+                   
+            }
+            Console.ReadLine();
+        }
+    }
+}
